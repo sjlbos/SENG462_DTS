f072fd75d39ee22ea94f5e2c7542b9f6a40c3cf5
Stephen Bos <sjlbos@outlook.com>
2016-03-22 21:30:42 -0700

    Added untested implementation of Mongo wrapper.

--- DTS/Services/TransactionMonitor/Repository/MongoDbAuditRepository.cs
+++ DTS/Services/TransactionMonitor/Repository/MongoDbAuditRepository.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using log4net;
+using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 using TransactionEvents;
 
@@ -11,57 +12,82 @@ namespace TransactionMonitor.Repository
     {
         private static ILog Log = LogManager.GetLogger(typeof (MongoDbAuditRepository));
 
-        private readonly IMongoClient _mongoClient;
-        private readonly IMongoDatabase _auditDb;
+        private readonly IMongoCollection<TransactionEvent> _eventCollection; 
 
-        public MongoDbAuditRepository(string connectionString)
+        public MongoDbAuditRepository(string connectionString, string eventCollectionName)
         {
             if(connectionString == null)
                 throw new ArgumentNullException("connectionString");
 
+            RegisterEventClasses();
+
             string databaseName = MongoUrl.Create(connectionString).DatabaseName;
-            _mongoClient = new MongoClient(connectionString);
-            _auditDb = _mongoClient.GetDatabase(databaseName);
+            var mongoClient = new MongoClient(connectionString);
+            var auditDb = mongoClient.GetDatabase(databaseName);
+            _eventCollection = auditDb.GetCollection<TransactionEvent>(eventCollectionName);
         }
+
+        private static void RegisterEventClasses()
+        {
+            BsonClassMap.RegisterClassMap<TransactionEvent>(cm =>
+            {
+                cm.AutoMap();
+                cm.SetIsRootClass(true);
+            });
+            BsonClassMap.RegisterClassMap<UserCommandEvent>();
+            BsonClassMap.RegisterClassMap<QuoteServerEvent>();
+            BsonClassMap.RegisterClassMap<AccountTransactionEvent>();
+            BsonClassMap.RegisterClassMap<SystemEvent>();
+            BsonClassMap.RegisterClassMap<ErrorEvent>();
+            BsonClassMap.RegisterClassMap<DebugEvent>();
+        }
+
+        #region IAuditRepository
 
         public void LogUserCommandEvent(UserCommandEvent userCommandEvent)
         {
-            throw new NotImplementedException();
+            _eventCollection.InsertOne(userCommandEvent);
         }
 
         public void LogQuoteServerEvent(QuoteServerEvent quoteServerEvent)
         {
-            throw new NotImplementedException();
+            _eventCollection.InsertOne(quoteServerEvent);
         }
 
         public void LogAccountTransactionEvent(AccountTransactionEvent accountTransactionEvent)
         {
-            throw new NotImplementedException();
+            _eventCollection.InsertOne(accountTransactionEvent);
         }
 
         public void LogSystemEvent(SystemEvent systemEvent)
         {
-            throw new NotImplementedException();
+            _eventCollection.InsertOne(systemEvent);
         }
 
         public void LogErrorEvent(ErrorEvent errorEvent)
         {
-            throw new NotImplementedException();
+            _eventCollection.InsertOne(errorEvent);
         }
 
         public void LogDebugEvent(DebugEvent debugEvent)
         {
-            throw new NotImplementedException();
+            _eventCollection.InsertOne(debugEvent);
         }
 
         public IEnumerable<TransactionEvent> GetLogsForUser(string userId, DateTime start, DateTime end)
         {
-            throw new NotImplementedException();
+            var builder = Builders<TransactionEvent>.Filter;
+            var filter = builder.Eq(e => e.UserId, userId) & builder.Gt(e => e.OccuredAt, start) & builder.Lt(e => e.OccuredAt, end);
+            return _eventCollection.Find(filter).ToEnumerable();
         }
 
         public IEnumerable<TransactionEvent> GetAllLogs(DateTime start, DateTime end)
         {
-            throw new NotImplementedException();
+            var builder = Builders<TransactionEvent>.Filter;
+            var filter = builder.Gt(e => e.OccuredAt, start) & builder.Lt(e => e.OccuredAt, end);
+            return _eventCollection.Find(filter).ToEnumerable();
         }
+
+        #endregion
     }
 }
