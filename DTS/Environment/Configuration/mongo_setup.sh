09cc0a642fb1a09dbb7165a914442162757d944e
Stephen Bos <sjlbos@outlook.com>
2016-03-23 12:25:42 -0700

    Minor changes to MongoDB scripts

--- DTS/Environment/Configuration/mongo_setup.sh
+++ DTS/Environment/Configuration/mongo_setup.sh
@@ -2,6 +2,7 @@
 
 INSTALL_ROOT=$1
 DATA_DIR=$2
+PORT=$3
 
 if [[ -z "$INSTALL_ROOT" ]]; then
 	echo "Mongo installation directory not set."
@@ -13,6 +14,11 @@ if [[ -z "$DATA_DIR" ]]; then
 	exit 1
 fi
 
+if [[ -z "$PORT" ]]; then
+    echo "MongoDb port not specified."
+    exit 1
+fi
+
 mkdir -p $DATA_DIR
 mkdir -p $DATA_DIR/db
 mkdir -p $DATA_DIR/log
@@ -20,13 +26,13 @@ mkdir -p $DATA_DIR/log
 cat << EOF > $DATA_DIR/mongod.cfg
 systemLog: 
     destination: file 
-    path: ./test/data/log/mongod.log 
+    path: $DATA_DIR/log/mongod.log 
 storage: 
-    dbPath: ./test/data/db 
+    dbPath: $DATA_DIR/db 
 net: 
-    port: 44410 
+    port: $PORT
     bindIp: 127.0.0.1 
-    maxIncomingConnections: 65536 
+    maxIncomingConnections: 500
 EOF
 
 $INSTALL_ROOT/bin/mongod --fork --config $DATA_DIR/mongod.cfg
