979b1e70f1cf058776dcb4f19f1d8219df5906da
Dustin Bolink <williambolink@gmail.com>
2016-03-26 16:57:22 -0700

    updated logging to see if API is problem

--- DTS/API/src/dtsapi/logger.go
+++ DTS/API/src/dtsapi/logger.go
@@ -9,15 +9,17 @@ import (
 
 func Logger(inner http.Handler, name string) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        println("Starting Request")
         start := time.Now()
 
         inner.ServeHTTP(w, r)
 
         log.Printf(
-		"%10s\t%10s\t%10s",
+		"%10s%20s%20s",
 		r.Header.Get("X-TransNo"),
 		name,
 		time.Since(start),
         )
+        println("Finished Request")
     })
 }
