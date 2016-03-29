d2ce0502692c81c94f1ce4a5cff880437d9b6112
sjlbos <sjlbos@outlook.com>
2016-03-28 19:46:17 -0700

    Changes to allow the Transaction Monitor to swap databases via config

--- DTS/Services/TransactionMonitor/TransactionMonitorService.cs
+++ DTS/Services/TransactionMonitor/TransactionMonitorService.cs
@@ -17,28 +17,44 @@ namespace TransactionMonitor
         private Uri _apiEndpoint;
         private string _eventCollectionName;
 
+        private IAuditRepository _repository;
+
         protected override void InitializeService()
         {
             _workerCount = Int32.Parse(ConfigurationManager.AppSettings["WorkerCount"]);
             _apiEndpoint = new Uri(ConfigurationManager.AppSettings["ApiRoot"]);
-            _dbConnectionString = ConfigurationManager.ConnectionStrings["MongoAuditDb"].ConnectionString;
-            _eventCollectionName = ConfigurationManager.AppSettings["EventCollectionName"];
+
+            string databaseToUse = ConfigurationManager.AppSettings["UseDatabase"];
+
+            if (String.Equals("MongoDB", databaseToUse))
+            {
+                string connnectionString = ConfigurationManager.ConnectionStrings["MongoAuditDB"].ConnectionString;
+                string collectionName = ConfigurationManager.AppSettings["EventCollectionName"];
+                _repository = new MongoDbAuditRepository(connnectionString, collectionName);
+            }
+            else if (String.Equals("Postgres", databaseToUse))
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["PostgresAuditDB"].ConnectionString;
+                _repository = new PostgresAuditRepository(connectionString);
+            }
+            else
+            {
+                throw new ConfigurationErrorsException("Invalid database type.");
+            }
         }
 
         protected override IList<IWorker> GetWorkerList()
         {
-     
             var workerList = new List<IWorker>();
-            var repository = new MongoDbAuditRepository(_dbConnectionString, _eventCollectionName);
 
             for (int i = 0; i < _workerCount; i++)
             {
                 var receiver = RabbitMessengerFactory.GetReceiver("TransactionEventQueueReceiver");
                 workerList.Add(new TransactionMonitorWorker(String.Format(CultureInfo.InvariantCulture,
-                    "Worker {0}", i), receiver, repository));
+                    "Worker {0}", i), receiver, _repository));
             }
 
-            workerList.Add(new NancyHostLauncherWorker("Nancy Launcher", _apiEndpoint, repository));
+            workerList.Add(new NancyHostLauncherWorker("Nancy Launcher", _apiEndpoint, _repository));
 
             return workerList;
         }
